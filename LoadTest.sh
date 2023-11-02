d493564880ee447b906faddbae844c956739032a
Pinapathruni Saikiran <109568252+saikiranpi@users.noreply.github.com>
2023-11-02 11:25:45 +0530

    Update LoadTest.sh

--- LoadTest.sh
+++ LoadTest.sh
@@ -1,3 +1,6 @@
+# Note: Replace the website address below “http://cmakkaya-test-ALB-for-WAF-173332589.us-east-1.elb.amazonaws.com/” 
+# with your own website address or Load Balancer’s endpoint where we are going to run the load testing.
+################
 #!/bin/bash
 for x in {1..200}
 do
